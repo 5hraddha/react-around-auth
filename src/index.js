995c018dfb039c49243b6647a5d2717c455678e2
Shraddha <mailmeatshraddha@gmail.com>
2022-02-07 00:02:39 -0500

    fix: add basename to BrowserRouter

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/react-around-auth">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
